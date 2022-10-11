5066dd9ddd9247130544042f576d011e1655637d
LAPTOP-HKB7FIRP\최준우 <jun_chois@naver.com>
2022-10-11 18:10:14 +0900

    Response Header w/ Set-Cookie 2

--- src/main/java/com/gitpher/lab1/HttpPracController.java
+++ src/main/java/com/gitpher/lab1/HttpPracController.java
@@ -1,6 +1,5 @@
 package com.gitpher.lab1;
 
-import com.sun.net.httpserver.HttpServer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +16,13 @@ public class HttpPracController {
     public ResponseEntity<?> home(HttpServletRequest httpServletRequest) {
 
         System.out.println("I'm home");
+
+        System.out.println("Method: " + httpServletRequest.getMethod());
+        System.out.println("AuthType: " + httpServletRequest.getAuthType());
+        System.out.println("ServletPath: " + httpServletRequest.getServletPath());
+        System.out.println("LocalName: " + httpServletRequest.getLocalName());
+
+
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Set-Cookie", "user=Jay; expires=Sat, 12-Oct-2022 00:00:00 KST; path=/;");
